c6b4872296013d3873a3454cf9b97fdf37643bb2
TauTey <kkozin897@gmail.com>
2025-11-19 14:55:23 +0530

    12

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -25,6 +25,62 @@ namespace Kozin41
             InitializeComponent();
             var currentProducts = Kozin41Entities.GetContext().Product.ToList();
             ProductListView.ItemsSource = currentProducts;
+
+            ComboType.SelectedIndex = 0;
+            TextItemCount.Text = $"{currentProducts.Count} из {Kozin41Entities.GetContext().Product.Count()}";
         }
+
+        private void UpdateProducts()
+        {
+            var currentProducts = Kozin41Entities.GetContext().Product.ToList();
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 9.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) <= 14.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+            }
+            currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (RButtonDown.IsChecked == true)
+            {
+                currentProducts = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
+            }
+            if (RButtonUp.IsChecked == true)
+            {
+                currentProducts = currentProducts.OrderBy(p => p.ProductCost).ToList();
+            }
+            ProductListView.ItemsSource = currentProducts;
+            TextItemCount.Text = $"{currentProducts.Count} из {Kozin41Entities.GetContext().Product.Count()}";
+        }
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        
     }
 }
